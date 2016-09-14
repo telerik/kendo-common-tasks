e6dfd746ef1d45105c3811068e2aa5b61469e38c
Petyo Ivanov <underlog@gmail.com>
2016-09-14 10:33:54 +0300

    chore: pin angular version

--- docs-public/plunker/systemjs.config.js
+++ docs-public/plunker/systemjs.config.js
@@ -1,4 +1,6 @@
 (function (global) {
+  var angularVersion = '2.0.0-rc.6';
+
   System.config({
     // DEMO ONLY! REAL CODE SHOULD NOT TRANSPILE IN THE BROWSER
     transpiler: 'ts',
@@ -22,24 +24,14 @@
       '@telerik': '#= npmUrl #@telerik',
 
       // angular bundles
-      '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
-      '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
-      '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
-      '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
-      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-      '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
-      '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
-      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-
-      // angular testing umd bundles
-      '@angular/core/testing': 'npm:@angular/core/bundles/core-testing.umd.js',
-      '@angular/common/testing': 'npm:@angular/common/bundles/common-testing.umd.js',
-      '@angular/compiler/testing': 'npm:@angular/compiler/bundles/compiler-testing.umd.js',
-      '@angular/platform-browser/testing': 'npm:@angular/platform-browser/bundles/platform-browser-testing.umd.js',
-      '@angular/platform-browser-dynamic/testing': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic-testing.umd.js',
-      '@angular/http/testing': 'npm:@angular/http/bundles/http-testing.umd.js',
-      '@angular/router/testing': 'npm:@angular/router/bundles/router-testing.umd.js',
-      '@angular/forms/testing': 'npm:@angular/forms/bundles/forms-testing.umd.js',
+      '@angular/core': 'npm:@angular/core@' + angularVersion +'/bundles/core.umd.js',
+      '@angular/common': 'npm:@angular/common@' + angularVersion +'/bundles/common.umd.js',
+      '@angular/compiler': 'npm:@angular/compiler@' + angularVersion +'/bundles/compiler.umd.js',
+      '@angular/platform-browser': 'npm:@angular/platform-browser@' + angularVersion +'/bundles/platform-browser.umd.js',
+      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic@' + angularVersion +'/bundles/platform-browser-dynamic.umd.js',
+      '@angular/http': 'npm:@angular/http@' + angularVersion +'/bundles/http.umd.js',
+      '@angular/forms': 'npm:@angular/forms@' + angularVersion + '/bundles/forms.umd.js',
+      '@angular/router': 'npm:@angular/router@3.0.0-rc.3/bundles/router.umd.js',
 
       // other libraries
       'rxjs':                       'npm:rxjs',
