5ec283825e2f7b48d39c53ca2355f71c87af2336
Alex Gyoshev <alex@gyoshev.net>
2017-03-23 11:03:58 +0200

    fix: center loading indicator

--- docs-public/snippets.js
+++ docs-public/snippets.js
@@ -249,7 +249,7 @@ var angularTemplate = kendo.template(
         html, body { overflow: hidden; }\
         body { font-family: "RobotoRegular",Helvetica,Arial,sans-serif; font-size: 14px; margin: 0; }\
         my-app { display: block; width: 100%; overflow: hidden; min-height: 80px; box-sizing: border-box; padding: 30px; }\
-        my-app > .k-loading { position: absolute; top: 10%; left: 50%; transform: translate(-50%, -50%); }\
+        my-app > .k-loading { position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); }\
         .example-wrapper { min-height: 280px; }\
         .example-wrapper p, .example-col p { margin: 20px 0 10px; }\
         .example-wrapper p:first-child, .example-col p:first-child { margin-top: 0; }\
