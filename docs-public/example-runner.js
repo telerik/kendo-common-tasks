fc3a7ed54283fb53ef8c885812e6fce75a3fe054
Alex Gyoshev <alex@gyoshev.net>
2018-01-24 11:24:21 +0200

    fix: bump minor version of loaded rxjs bundle
    adds missing pipable operators, part of
    https://github.com/telerik/kendo-angular/issues/1244

--- docs-public/example-runner.js
+++ docs-public/example-runner.js
@@ -222,7 +222,7 @@ window.ExampleRunner = (function() {
                     suppressImplicitAnyIndexErrors: true
                 },
                 bundles: {
-                    "https://unpkg.com/rxjs-system-bundle@5.5.2/Rx.system.min.js": [
+                    "https://unpkg.com/rxjs-system-bundle@5.5.6/Rx.system.min.js": [
                         "rxjs",
                         "rxjs/*",
                         "rxjs/operator/*",
