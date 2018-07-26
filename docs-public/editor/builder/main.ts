0b0cccd1998d00629d1dedc4e08f96f2a0ad4249
Alex Gyoshev <alex@gyoshev.net>
2018-07-26 17:33:23 +0300

    fix(builder): enable hot reloading in stackblitz

--- docs-public/editor/builder/main.ts
+++ docs-public/editor/builder/main.ts
@@ -3,5 +3,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
+  // Ensure Angular destroys itself on hot reloads.
+  if (window['ngRef']) {
+    window['ngRef'].destroy();
+  }
+  window['ngRef'] = ref;
+
+  // Otherise, log the boot error
+}).catch(err => console.error(err));
